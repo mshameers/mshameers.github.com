a45b6b434bb7b03d05cb0f144f24b9e6b7d2fdab
mshameers <m.shameer.s@gmail.com>
2012-07-14 10:30:55 +0530

    comments changed

--- commentsubmit.php
+++ commentsubmit.php
@@ -62,7 +62,6 @@ foreach ($_POST as $key => $value) {
 if (mail($EMAIL_ADDRESS, $SUBJECT, $msg, "From: $EMAIL_ADDRESS"))
 {
 	include $COMMENT_RECEIVED;
-	{% include comment_received.html %}
 
 }
 else
